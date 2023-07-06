433e368186bf80d2aa7c37778f8fe3142d5d860b
FionaG26 <fionamuthoni18@gmail.com>
2023-07-06 10:18:15 +0000

    4. Define variables

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+"""
+Module: 4-define_variables
+Description: Defines and annotates variables with specific values.
+"""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
