6cda67c53dc84a5f261fcc4fe1279b74fff49480
FionaG26 <fionamuthoni18@gmail.com>
2023-07-06 11:48:30 +0000

    12. Type Checking

--- 0x00-python_variable_annotations/102-type_checking.py
+++ 0x00-python_variable_annotations/102-type_checking.py
@@ -8,26 +8,17 @@ Contains the zoom_array function that zooms in an array by a given factor.
 from typing import List, Tuple
 
 
-def zoom_array(lst: List[int], factor: int = 2) -> List[int]:
-    """
-    Zooms in the input array by the given factor.
-
-    Args:
-        lst: The input array.
-        factor: The zoom factor (default is 2).
-
-    Returns:
-        The zoomed-in array.
-    """
-    zoomed_in = [
-        item
-        for item in lst
-        for _ in range(factor)
+def zoom_array(lst: Tuple, factor: int = 2) -> List:
+    '''Creates multiple copies of items in a tuple.
+    '''
+    zoomed_in: List = [
+        item for item in lst
+        for i in range(int(factor))
     ]
     return zoomed_in
 
 
-array = [12, 72, 91]
+array = (12, 72, 91)
 
 zoom_2x = zoom_array(array)
 
